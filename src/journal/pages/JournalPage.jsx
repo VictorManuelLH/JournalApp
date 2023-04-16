4a76b9cd76cca5225eadcf1940470e6d7d71cbe7
VictorManuelLH <victormanuelluishernandez@gmail.com>
2023-04-16 00:15:41 -0600

    Diseño terminado de JournalApp

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,25 @@
-import { Typography } from "@mui/material"
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ab molestiae quas, ipsum optio libero repellat dolores commodi necessitatibus quos, rerum, accusantium consequuntur eveniet blanditiis nihil quis vitae perferendis? Blanditiis?</Typography>
+            {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ab molestiae quas, ipsum optio libero repellat dolores commodi necessitatibus quos, rerum, accusantium consequuntur eveniet blanditiis nihil quis vitae perferendis? Blanditiis?</Typography> */}
+            {/* <NothingSelectedView /> */}
+            <NoteView />
+            <IconButton 
+                size="large"
+                sx={{ color: 'white',
+                    backgroundColor: 'error.main', 
+                    ':hover': { backgroundColor: 'error.main', 
+                        opacity: 0.9 }, 
+                        position: 'fixed', 
+                        right: 50, 
+                        bottom: 50 }}>
+                <AddOutlined sx={{ fontSize: 30 }} />
+            </IconButton>
         </JournalLayout>
     )
 }
