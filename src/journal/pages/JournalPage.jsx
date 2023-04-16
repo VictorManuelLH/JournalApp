229490c74f0f376aecaecdb288335cdbb75d3eb6
VictorManuelLH <victormanuelluishernandez@gmail.com>
2023-04-15 21:45:25 -0600

    Navbar y SideBar agregados

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,10 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
     return (
-        <>
-            <Typography variant='h1' >JournalPage</Typography>
-        </>
+        <JournalLayout>
+            <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ab molestiae quas, ipsum optio libero repellat dolores commodi necessitatibus quos, rerum, accusantium consequuntur eveniet blanditiis nihil quis vitae perferendis? Blanditiis?</Typography>
+        </JournalLayout>
     )
 }
