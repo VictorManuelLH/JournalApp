6114d3f5882eb1fdad3f9525d65765c0bf0ed48c
VictorManuelLH <victormanuelluishernandez@gmail.com>
2023-05-14 00:35:00 -0600

    Commit resubido

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,16 +1,23 @@
 import { Box } from "@mui/system"
 import { Navbar, SideBar } from "../components"
 import { Toolbar } from "@mui/material"
+import { useState } from "react";
 
 const drawerWidth = 240
 
 export const JournalLayout = ({ children }) => {
+
+    const [mobileOpen, setMobileOpen] = useState(false);
+    const handleDrawerToggle = () => {
+            setMobileOpen(!mobileOpen);
+        }
+
     return (
         <Box sx={{ display: 'flex' }} className="animate__animated animate__fadeIn animate__faster" >
 
-            <Navbar drawerWidth={ drawerWidth } />
+            <Navbar drawerWidth={drawerWidth} setOpen={handleDrawerToggle} />
 
-            <SideBar drawerWidth={ drawerWidth } />
+            <SideBar drawerWidth={drawerWidth} openSidebar={mobileOpen} setOpen={handleDrawerToggle} />
 
             <Box 
                 component="main" 
